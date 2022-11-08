8a58655ff35d3454f94cdea280dca479fc703e03
stannoob <85557566+stannoob@users.noreply.github.com>
2022-11-08 21:35:10 +0700

    Add files via upload

---
+++ controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $data = array('title' => 'Home');
+        return view('content', $data);
+    }
+}
