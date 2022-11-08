40124322449256d59bea1b2f89d4fb22c933fb2d
stannoob <85557566+stannoob@users.noreply.github.com>
2022-11-08 21:37:00 +0700

    Add files via upload

---
+++ views/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="container bg-secondary">
+
+        <a class="navbar-brand" href="/">GSLC WEBPROG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="mr-auto navbar-nav"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
